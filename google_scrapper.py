724a8ed93e23c3475def12073d3c0c50c07ea142
atchi102 <atchi102@mail.chapman.edu>
2018-03-07 16:30:39 -0800

    made final edits to program to account for duplicates

--- google_scrapper.py
+++ google_scrapper.py
@@ -1,3 +1,4 @@
+from math import cos, asin, sqrt
 import argparse
 import json
 import pprint
@@ -22,16 +23,16 @@ API_HOST = 'https://maps.googleapis.com/maps/api/place'
 API_KEY = 'AIzaSyB64uYpz5jkKuLJsPGNKSRiOzpApaKfNEs'
 SEARCH_PATH = '/textsearch/json?'
 PLACE_DETAIL_PATH = '/details/json?'  # Business ID will come after slash.
-LATLONG = [(42,124),(42,123),(42,122),(42,121),(42,120),
-           (41,124),(41,123),(41,122),(41,121),(41,120),
-           (40,124),(40,123),(40,122),(40,121),(40,120),
-                    (39,123),(39,122),(39,121),(39,120),
-                    (38,123),(38,122),(38,121),(38,120),(38,119),
-                             (37,122),(37,121),(37,120),(37,119),(37,118),(37,117),
-                             (36,122),(36,121),(36,120),(36,119),(36,118),(36,117),(36,116),
-                                               (35,120),(35,119),(35,118),(35,117),(35,116),(35,115),
-                                                        (34,119),(34,118),(34,117),(34,116),(34,115),
-                                                                 (33,118),(33,117),(33,116),(33,115)]
+LATLONG = [(42,-124),(42,-123),(42,-122),(42,-121),(42,-120),
+           (41,-124),(41,-123),(41,-122),(41,-121),(41,-120),
+           (40,-124),(40,-123),(40,-122),(40,-121),(40,-120),
+                     (39,-123),(39,-122),(39,-121),(39,-120),
+                     (38,-123),(38,-122),(38,-121),(38,-120),(38,-119),
+                               (37,-122),(37,-121),(37,-120),(37,-119),(37,-118),(37,-117),
+                               (36,-122),(36,-121),(36,-120),(36,-119),(36,-118),(36,-117),(36,-116),
+                                                   (35,-120),(35,-119),(35,-118),(35,-117),(35,-116),(35,-115),
+                                                             (34,-119),(34,-118),(34,-117),(34,-116),(34,-115),
+                                                                       (33,-118),(33,-117),(33,-116),(33,-115)]
 RADIUS = 50000
 
 D = {}
@@ -63,7 +64,7 @@ def request(host, path, url_params=None):
 
     return response.json()
 
-def search(api_key, lat, lon, radius):
+def search(word,api_key, lat, lon, radius):
     """Query the Search API by a search term and location.
     Args:
         term (str): The search term passed to the API.
@@ -73,74 +74,94 @@ def search(api_key, lat, lon, radius):
     """
 
     url_params = {
-        'query': 'ecig',
+        'query': word,
         'location': '{0},{1}'.format(lat,lon),
         'radius': radius,
         'key': api_key
     }
     return request(API_HOST, SEARCH_PATH, url_params=url_params)
 
-def query_api(lat, lon):
+def query_api(lat, lon, word, compare):
     """Queries the API by the input values from the user.
     Args:
         zip_code (str): The zip_code of the business to query.
     """
-    response = search(API_KEY, lat, lon, RADIUS)
+    response = search(word,API_KEY, lat, lon, RADIUS)
     #print response
     businesses = response.get('results')
 
-    for business in businesses:
-        print("Name: {0} ID: {1} Types: {2}".format(business['name'],business['id'],business['types']))
+    dis = 0
 
-    # if not businesses:
-    #     print(u'No businesses for __ in {1} found.'.format(zip_code))
-    #     return
-    #
-    # print zip_code, len(businesses)
-    #
-    # with open('yelp_results.csv','a') as f:
-    #     for business in businesses:
-    #         if business['id'].encode('utf-8').strip() not in D:
-    #             if not business['is_closed']:
-    #                 D[business['id'].encode('utf-8').strip()] = 1
-    #                 categories = ''
-    #                 for cat_dict in business['categories']:
-    #                     categories += cat_dict['alias'] + ' '
-    #                 results = {
-    #                     'id':business['id'].encode('utf-8').strip(),
-    #                     'lat':business['coordinates']['latitude'],
-    #                     'long':business['coordinates']['longitude'],
-    #                     'name':business['name'].encode('utf-8').strip(),
-    #                     'address':u' '.join(business['location']['display_address']).encode('utf-8').strip().replace(',',''),
-    #                     'category':categories.encode('utf-8').strip(),
-    #                     'zip':zip_code.encode('utf-8').strip(),
-    #                     'phone':business['phone'].encode('utf-8').strip(),
-    #                     'closed':business['is_closed']
-    #                     }
-    #                 line = '{id},{lat},{long},{name},{address},{category},{zip},{phone},{closed}\n'.format(**results)
-    #                 f.write(line)
+    print('{3} businesses for {0} in {1},{2} found.'.format(word,lat,lon,len(businesses)))
+
+    with open('google_results_unique.csv','a') as u:
+        with open('google_results_all.csv','a') as a:
+            for business in businesses:
+                if CA in business['formatted_address']:
+                    if business['id'].encode('utf-8').strip() not in D:
+                        D[business['id'].encode('utf-8').strip()] = 1
+                        results = {
+                            'id':business['id'].encode('utf-8').strip(),
+                            'lat':business['geometry']['location']['lat'],
+                            'long':business['geometry']['location']['lng'],
+                            'name':business['name'].encode('utf-8').strip(),
+                            'address':business['formatted_address'].strip().replace(',',''),
+                            'category':word.encode('utf-8').strip(),
+                            }
+                        line = '{id},{lat},{long},{name},{address},{category}\n'.format(**results)
+                        for name, la, lo in compare:
+                            if(la == 'None' or lo == 'None' or business['geometry']['location']['lat']== 'None' or  business['geometry']['location']['lng']== 'None'):
+                                continue
+
+                            dis = abs(distance(la,lo,business['geometry']['location']['lat'],business['geometry']['location']['lng']))
+                            if(name == business['name'] and dis <= 1):
+                                a.write(line)
+                                break
+
+                            if dis <= 0.07:
+                                a.write(line)
+                                break
+                        if dis > 0.7:
+                            print line
+                            a.write(line)
+                            u.write(line)
+
+
+def distance(lat1, lon1, lat2, lon2):
+    lat1 = float(lat1)
+    lon1 = float(lon1)
+    lat2 = float(lat2)
+    lon2 = float(lon2)
+    p = 0.017453292519943295     #Pi/180
+    a = 0.5 - cos((lat2 - lat1) * p)/2 + cos(lat1 * p) * cos(lat2 * p) * (1 - cos((lon2 - lon1) * p)) / 2
+    return 12742 * asin(sqrt(a)) #2*R*asin...
+
 
 def main():
 
     #read in Yelp YelpResults
     df = pandas.read_csv('yelp_results.csv')
+    exisitng_names = df.name
     existing_latitudes = df.lat
     exisiting_longitudes = df.long
-    exisiting_latlongs = zip(existing_latitudes,exisiting_longitudes)
-    print(exisiting_latlongs)
+    exisiting_latlongs = zip(exisitng_names, existing_latitudes,exisiting_longitudes)
 
     # write header line
-    # with open('google_results.csv','w') as f:
-    #     f.write('id,lat,long,name,address,zip,phone,closed\n')
-    #
-    # query_api(34.0522,-118.2437)
+    with open('google_results_all.csv','w') as f1:
+        f1.write('id,lat,long,name,address,word\n')
 
-    # for lat,lon in LATLONG:
-    #     print("LAT {0} LONG {1}".format(lat,lon))
-    #     try:
-    #         query_api(lat,lon)
-    #     except Exception, e:
-    #         print e
+    with open('google_results_unique.csv','w') as f2:
+        f2.write('id,lat,long,name,address,word\n')
+
+    keywords = ['ecig','ecigarette','vape','vapor','vaper','vapin','vaping','electronic+cigarette']
+
+    for word in keywords:
+        for lat,lon in LATLONG:
+            print("LAT {0} LONG {1}".format(lat,lon))
+            try:
+                query_api(lat,lon,word,exisiting_latlongs)
+            except Exception, e:
+                print e
 
 
 if __name__ == '__main__':
